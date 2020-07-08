3bbf6f2c292b71714acba5d45b8620b0b8feaacf
nathanprows77 <66317477+nathanprows77@users.noreply.github.com>
2020-07-08 00:45:27 -0400

    update pypoll

--- PyBank/Analysis/main.py
+++ PyBank/Analysis/main.py
@@ -1,11 +1,47 @@
 import os
 import csv
 
-# csvpath = os.path.join('..', 'Analysis', 'Unit_03_Python_Instructions_PyBank_Resources_budget_data (1).csv')
+total = 0
+month = 0
+csvpath = os.path.join("PyBank", "Resources", "budget_data.csv")
 
-# with open(csvpath) as csv_file:
-#     csv_reader = csv.reader(csv_file, delimiter=',')
-#     print(csv_reader)
+Date = []
+Profit_losses = []
 
-print('Financial Analysis')
-print("-------------------------------------")
+with open(csvpath) as csv_file:
+    csv_reader = csv.reader(csv_file, delimiter=',')
+    
+    csv_header = next(csv_reader)
+    previous_month = next(csv_reader)[1]
+    total = int(previous_month)
+    max_for_loop = 0
+    max_month = ""
+    min_for_loop = 0
+    min_month = ""
+    for row in csv_reader:
+        Date.append(row[0])
+        Profit_losses.append(int(row[1]) - (int(previous_month)))
+        previous_month = row[1]
+        total += int(row[1])
+        if int(row[1]) >= 0:
+            max_for_loop = int(row[1])
+            max_month = row[0]
+        if int(row[1]) <= 0:
+            min_for_loop = int(row[1])
+            min_month = row[0]
+    month = len(Profit_losses) + 1     
+    average_change = sum(Profit_losses)/len(Profit_losses)
+    max_increase = max(Profit_losses)
+    min_increase = min(Profit_losses)
+
+
+   
+    print('Financial Analysis')
+    print("-------------------------------------")
+    print("Total Months: " + str(month))
+    print("Total:", ('${0}'.format(format(total))))
+    print("Total Average Change:", ('${0}'.format(format(average_change, ',.2f'))))
+    print("Greatest Monthly Increase in Profits " + max_month + " ($" + str(max_increase) + ")")
+    print("Greatest Monthly Increase in Profits " + min_month + " ($" + str(min_increase) + ")")
+
+    
--- PyPoll/Analysis/main.py
+++ PyPoll/Analysis/main.py
@@ -1,10 +1,61 @@
 import os
 import csv
 
+total = 0
+candidate = 0
+votes = 0
+Vote_percent = 0
+
+#Import csv file
 poll_csv = os.path.join("PyPoll", "Resources", "election_data.csv")
 
+#Define columns as lists
+Voter_ID = []
+County = []
+Candidate = []
+Unique_Candidate_List = []
+Vote_percent = {}
+Vote_percent = dict()
+#Open csv file with csvreader
 with open(poll_csv) as csvfile:
-    csvreader = csv.reader(cvsfile, delimiter=",")
-
+    csvreader = csv.reader(csvfile, delimiter=",")
     csv_header = next(csvfile)
-    print(f"Header: {csv_header}")
+    # previous_candidates = next(csvreader)[2]
+    Total_Votes = 0
+    Vote_count = {}
+    Vote_count = dict()
+
+    for row in csvreader:
+        Voter_ID.append(row[0])
+        County.append(row[1])
+        Candidate.append(row[2])
+        # previous_candidates = row[2]
+        Total_Votes = total + len(Candidate)
+        
+         
+        if row[2] not in Unique_Candidate_List:
+            Unique_Candidate_List.append(row[2])
+
+        if row[2] not in Vote_count.keys():
+            Vote_count[row[2]] = 1
+        else:
+            Vote_count[row[2]] += 1
+
+  
+print("Election Results")
+print("------------------------------------------------------")
+print("Total Votes: ", Total_Votes)
+print("------------------------------------------------------")
+high_score = 0
+winner = ""
+for candidate in Unique_Candidate_List:
+    Vote_percent[candidate] = (Vote_count[candidate] / Total_Votes) * 100
+    print(candidate + ": " + str(round(Vote_percent[candidate], 3)) + "% (" + str(Vote_count[candidate]) + ")")
+    if Vote_count[candidate] > high_score:
+        high_score = Vote_count[candidate]
+        winner = candidate
+print("------------------------------------------------------")
+print("Winner: " + winner)
+print("------------------------------------------------------")
+
+python /PyPoll/Analysis/main.py > /Desktop/pypoll.txt
